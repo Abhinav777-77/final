149116d6c0780914c93505de17e97afc0c9c3af8
Abhinav777-77 <varlaabhinav7@gmail.com>
2025-05-31 19:07:41 +0530

    Update 2ndstepVerification.tsx

--- frontend/src/pages/2ndstepVerification.tsx
+++ frontend/src/pages/2ndstepVerification.tsx
@@ -130,7 +130,7 @@
         console.log("Sending OTP to:", email);
         
         const response = await axios.post(
-          "http://localhost:5000/api/otp/resend-otp",
+          "https://Abhinav777-77--fl.hf.space/api/otp/resend-otp",
           { email },
           {
             headers: { 
@@ -205,7 +205,7 @@
         console.log("Resending OTP to:", email);
         
         const response = await axios.post(
-          "http://localhost:5000/api/otp/resend-otp",
+          "https://Abhinav777-77--fl.hf.space/api/otp/resend-otp",
           { email },
           {
             headers: { 
@@ -282,7 +282,7 @@
       try {
         const token = sessionStorage.getItem("authToken");
         const response = await axios.post(
-          "http://localhost:5000/api/otp/verify-otp",
+          "https://Abhinav777-77--fl.hf.space/api/otp/verify-otp",
           { otp },
           {
             headers: { "x-auth-token": token },
